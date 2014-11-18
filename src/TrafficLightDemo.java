4a0f3d7006137a27ebf1a6470733f3e84e0fb47e
tetsuo-shima <dfrancis0@gmail.com>
2014-11-18 13:09:10 -0500

    Header added to Demo

--- src/TrafficLightDemo.java
+++ src/TrafficLightDemo.java
@@ -3,6 +3,8 @@
  */
 public class TrafficLightDemo {
     public static void main(String[] args) {
+        System.out.println("Traffic Light Times:");
+
         for (TrafficLight light : TrafficLight.values()) {
             System.out.printf("%-10s%d seconds\n", light, light.getTime());
         }
